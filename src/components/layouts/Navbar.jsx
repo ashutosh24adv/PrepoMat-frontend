1f98b94033235d3ed69a8466d1b0de6153919724
ashutosh24adv <165794255+ashutosh24adv@users.noreply.github.com>
2025-07-25 15:15:33 +0530

    Update Navbar.jsx

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="h-16 bg-white border boredr-b border-gray-200/50 backdrop-blur-[2px] py-2.5 px-4 md:px-0 sticky top-0 z-30">
       <div className="container mx-auto flex items-center justify-between gap-5">
         <Link to="/dashboard">
-           <h2 className="text-xl md:text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#ff9324] via-orange-400 to-yellow-300 tracking-wide transform hover:scale-105 hover:rotate-[0.5deg] transition-all duration-300 ease-in-out drop-shadow-sm">
+           <h2 className="text-lg md:text-xl font-bold leading-5 text-[#ff9324] tracking-wide hover:scale-105 transition-transform duration-200">
     PrepoMat
   </h2>
         </Link>
